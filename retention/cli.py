269e63baea8b21d4e6fa35f2c13a280db6778858
murad <murad2000balushi@gmail.com>
2025-09-26 23:51:45 +0400

    feat: Enhance transcription functionality in CLI by adding file processing and output handling

--- retention/cli.py
+++ retention/cli.py
@@ -1,7 +1,8 @@
 import typer
 from pathlib import Path
+import whisper
 
-
+model = whisper.load_model("base")
 app = typer.Typer()
 
 
@@ -11,7 +12,11 @@ def run(lecture: str):
 
     path = Path(lecture)
     if path.exists():
-        print (f"Got file: {lecture}")
+        print (f"Got file: {lecture} \n Transcribing....")
+        result = model.transcribe(lecture)
+        with open("output.txt", "w", encoding="utf-8") as f:
+            f.write(result["text"]) # type: ignore
+       
     else:
         print (f"file not found: {lecture}")
 
